67ad012a786fb9ed495ca5e8fd5a1a94f3a11049
Natalia Rukhlyadko <Natalia-ASSE@yandex.ru>
2023-04-02 11:43:42 +0300

    homework_JS_10

---
+++ 10.3/script.js
@@ -0,0 +1,75 @@
+const wsUri = "wss://echo-ws-service.herokuapp.com";
+
+const infoOutput = document.querySelector(".info_output");
+const chatOutput = document.querySelector(".chat");
+const input = document.querySelector("input");
+const sendBtn = document.querySelector(".btn");
+const btn_geo = document.querySelector(".btn_geo");
+
+function pageLoaded() {
+
+    
+  let socket = new WebSocket(wsUri);
+    
+  socket.onopen = () => {
+    infoOutput.innerText = "Соединение установлено";
+  }
+    
+  socket.onmessage = (event) => {
+    writeToChat(event.data, true);
+  }
+    
+  socket.onerror = () => {
+    infoOutput.innerText = "При передаче данных произошла ошибка";
+  }
+    
+  sendBtn.addEventListener("click", sendMessage);
+    
+  function sendMessage() {
+    if (!input.value) return;
+    socket.send(input.value);
+    writeToChat(input.value, false);
+    input.value === "";
+  }
+    
+  function writeToChat(message, isRecieved) {
+    let messageHTML = `<div class="${isRecieved? "recieved" : "sent"}">${message}</div>`;
+    chatOutput.innerHTML += messageHTML;
+  }
+  
+  
+  const error = () => {
+    infoOutput.textContent = 'Невозможно получить ваше местоположение';
+  }
+
+  const success = (position) => {
+    let latitude  = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let geoLink = `<a href = 'https://www.openstreetmap.org/#map=18/${latitude}/${longitude} target='_blank' 
+    style = 'background: #1b8a1b; align-self: flex-end; max-width: 80%;
+    padding: 5px 10px; margin: 5px 0; border-radius: 3px;
+    color: #fff;' )>Ваша гео-локация</a>`;
+    chatOutput.innerHTML += geoLink;
+    infoOutput.innerText = `Широта: ${latitude} °, Долгота: ${longitude} °`;
+  };
+
+
+  btn_geo.addEventListener('click', () => {
+    infoOutput.href = '';
+    infoOutput.textContent = '';
+  
+  if (!navigator.geolocation) {
+    infoOutput.textContent = 'Geolocation не поддерживается вашим браузером';
+  } else {
+    infoOutput.textContent = 'Определение местоположения…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  });
+
+
+}
+  
+  
+
+document.addEventListener("DOMContentLoaded", pageLoaded);
+
